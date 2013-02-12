b93e19e62835fbc9407f3df83ea2146505ddd5ea
Joonas Javanainen <joonas.javanainen@gmail.com>
2013-02-12 23:24:05 +0200

    New: Upgraded to Highcharts 2.3.5

--- highcharts/src/main/resources/web/splake/ext/highcharts-2.3.5/js/modules/canvas-tools.js
+++ highcharts/src/main/resources/web/splake/ext/highcharts-2.3.5/js/modules/canvas-tools.js
@@ -11,7 +11,7 @@
 
  Requires: rgbcolor.js - http://www.phpied.com/rgb-color-parser-in-javascript/
 
- Highcharts JS v2.3.3 (2012-10-04)
+ Highcharts JS v2.3.5 (2012-12-19)
  CanVGRenderer Extension module
 
  (c) 2011-2012 Torstein Hønsi, Erik Olsson
