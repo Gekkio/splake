0f0e5c7058164dc28a21442b663112b9492e4033
Joonas Javanainen <joonas.javanainen@gmail.com>
2013-02-13 02:40:56 +0200

    Oops: Chmor o_O

--- scripts/chromedriver_linux32.sh
+++ scripts/chromedriver_linux32.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 wget -q -O- http://chromedriver.googlecode.com/files/chromedriver_linux32_26.0.1383.0.zip | funzip > /usr/bin/chromedriver
-chmor +x /usr/bin/chromedriver
+chmod +x /usr/bin/chromedriver
