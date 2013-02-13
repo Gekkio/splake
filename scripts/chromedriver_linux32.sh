971af77ce31747daaf2ac3201e15fb5fd74ccaf5
Joonas Javanainen <joonas.javanainen@gmail.com>
2013-02-13 02:39:13 +0200

    Fix: Make sure chromedriver is executable

--- scripts/chromedriver_linux32.sh
+++ scripts/chromedriver_linux32.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 
-wget -O- http://chromedriver.googlecode.com/files/chromedriver_linux32_26.0.1383.0.zip | funzip > /usr/bin/chromedriver
+wget -q -O- http://chromedriver.googlecode.com/files/chromedriver_linux32_26.0.1383.0.zip | funzip > /usr/bin/chromedriver
+chmor +x /usr/bin/chromedriver
